c5dcdf95f768afad516191dc0ce6bd659b84c130
Matt Riese <rise.matt@gmail.com>
2021-06-10 09:52:30 -0700

    larger form has cors problems

--- src/App.js
+++ src/App.js
@@ -7,7 +7,12 @@ import axios from 'axios';
 
 function App() {
   const [signupData, setSignupData] = useState({
-    file: null
+    username: '',
+    email: '',
+    password: '',
+    bio: '',
+    location: '',
+    file: undefined
   });
 
   function handleChange(evt) {
@@ -21,17 +26,85 @@ function App() {
   async function handleSubmit(evt) {
     evt.preventDefault()
     try {
-      let resp = await axios.post("localhost:5000/", signupData);
-      console.log("axios resp=", resp)
-;    } catch (err) {
+      console.log("signupData in react handlesubmit===>>>>", signupData);
+      let resp = await axios.post(
+                            "http://localhost:5000/signup",
+                            signupData,
+                            { headers: {"Content-type": "multipart/form-data",
+                                        "Access-Control-Allow-Origin" : "*"} });
+      console.log("axios resp=", resp);
+    } catch (err) {
       console.warn("error= ", err);
     }
   }
   return (
     <div className="App">
       <Form onSubmit={handleSubmit}>
-
-
+        <Form.Group>
+          <Form.Label htmlFor="signup-username">Username: </Form.Label>
+          <Form.Control
+            className="mb-2 mr-sm-2"
+            name="username"
+            value={signupData.username}
+            id="signup-username"
+            type="text"
+            placeholder="username"
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="signup-email">Email: </Form.Label>
+          <Form.Control
+            className="mb-2 mr-sm-2"
+            name="email"
+            value={signupData.email}
+            id="signup-email"
+            type="email"
+            placeholder="email"
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="signup-password">Password: </Form.Label>
+          <Form.Control
+            className="mb-2 mr-sm-2"
+            name="password"
+            value={signupData.password}
+            id="signup-password"
+            type="password"
+            placeholder="password"
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="signup-bio">Bio: </Form.Label>
+          <Form.Control
+            className="mb-2 mr-sm-2"
+            name="bio"
+            value={signupData.bio}
+            id="signup-bio"
+            type="text"
+            placeholder="bio"
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+        <Form.Group>
+          <Form.Label htmlFor="signup-location">Location: </Form.Label>
+          <Form.Control
+            className="mb-2 mr-sm-2"
+            name="location"
+            value={signupData.location}
+            id="signup-location"
+            type="text"
+            placeholder="location"
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
         <Form.Group>
           <Form.Label htmlFor="signup-file"></Form.Label>
           <Form.Control
@@ -49,76 +122,3 @@ function App() {
 }
 
 export default App;
-
-
-// username: '',
-// email: '',
-// password: '',
-// bio: '',
-// location: '',
-
-// <Form.Group>
-//         <Form.Label htmlFor="signup-username"></Form.Label>
-//         <Form.Control
-//           className="mb-2 mr-sm-2"
-//           name="username"
-//           value={signupData.username}
-//           id="signup-username"
-//           type="text"
-//           placeholder="username"
-//           onChange={handleChange}
-//           required
-//         />
-//       </Form.Group>
-//       <Form.Group>
-//         <Form.Label htmlFor="signup-email"></Form.Label>
-//         <Form.Control
-//           className="mb-2 mr-sm-2"
-//           name="email"
-//           value={signupData.email}
-//           id="signup-email"
-//           type="email"
-//           placeholder="email"
-//           onChange={handleChange}
-//           required
-//         />
-//       </Form.Group>
-//       <Form.Group>
-//         <Form.Label htmlFor="signup-password"></Form.Label>
-//         <Form.Control
-//           className="mb-2 mr-sm-2"
-//           name="password"
-//           value={signupData.password}
-//           id="signup-password"
-//           type="password"
-//           placeholder="password"
-//           onChange={handleChange}
-//           required
-//         />
-//       </Form.Group>
-//       <Form.Group>
-//         <Form.Label htmlFor="signup-bio"></Form.Label>
-//         <Form.Control
-//           className="mb-2 mr-sm-2"
-//           name="bio"
-//           value={signupData.bio}
-//           id="signup-bio"
-//           type="text"
-//           placeholder="bio"
-//           onChange={handleChange}
-//           required
-//         />
-//       </Form.Group>
-//       <Form.Group>
-//         <Form.Label htmlFor="signup-location"></Form.Label>
-//         <Form.Control
-//           className="mb-2 mr-sm-2"
-//           name="location"
-//           value={signupData.location}
-//           id="signup-location"
-//           type="text"
-//           placeholder="location"
-//           onChange={handleChange}
-//           required
-//         />
-//       </Form.Group>
